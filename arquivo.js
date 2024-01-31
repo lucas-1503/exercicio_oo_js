fca798eda6830f9e80b83d1777d51fe6452c16c7
Lucas Montezuma <lucas.montezumaa@gmail.com>
2024-01-31 19:08:37 -0300

    first commit

---
+++ arquivo.js
@@ -0,0 +1,27 @@
+function Racas(tipo,bom_contra,mal_contra,cor){
+    this.tipo = tipo;
+    this.bom_contra = bom_contra;
+    this.mal_contra = mal_contra;
+    this.cor = cor;
+}
+
+
+const elfos = new Racas("elfos","orcs","undeads","amarelo");
+const orcs = new Racas("orcs","undeads","elfos","verde");
+const undeads = new Racas("undeads","elfos","orcs","preto");
+
+function Personagem(nome,level,tipo){
+    this.nome = nome;
+    this.level = level;
+    
+    Racas.call(this,tipo)
+}
+
+
+const elfo1 = new Personagem("Joao",1,elfos)
+const orc1 = new Personagem("maria", 10, orcs)
+const und1 = new Personagem("Fernando",12,undeads)
+
+console.log(elfo1)
+console.log(orc1)
+console.log(und1)
